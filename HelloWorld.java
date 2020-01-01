f09bed063f8fe489a322e6f0eb0bf4b9171824df
Barry S Rigg <barrysrigg@gmail.com>
2019-12-31 23:17:46 -0500

    Added comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,13 @@
-// name of class must always be the same as the file name
+// name of class must always be the same as the file name (PASCAL Case - Starts with capital letter and every new word is capitalized.)
 public class HelloWorld{
-    // main method will always be the the starting point
+    // main method will always be the the starting point (Entry Point Method)
+    // void is used when nothing is returned from method that is run else instead of void you would have data type being returned.
+    // main has an Array of Strings (Strings[]) as its arguments (args)
+    // method names should be camelCase and convention is to use a verb to start ieach method except your ntry point method "main"
     public static void main(String[] args){
+        // the dot "." nottion is an OOP concept!!!
+        // System is a class that comes with the Java library and .out is the variable that return and object of the PrintStream class
+        // and .println is the method that accepts the String argument = "Hello World"
         System.out.println("Hello World");
     }
 
