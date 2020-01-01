f919ad745ccffc69f59275c565874ecbec4351cb
Barry S Rigg <barrysrigg@gmail.com>
2019-12-31 21:59:37 -0500

    first Commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// name of class must always be the same as the file name
+public class HelloWorld{
+    // main method will always be the the starting point
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
